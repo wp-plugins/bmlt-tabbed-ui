3bcd8d1fd1911ec73afa41a4a35b956413fc8f46
odathp <odathp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-11 04:37:30 +0000

    Tagging version 4.2
    git-svn-id: https://plugins.svn.wordpress.org/bmlt-tabbed-ui/tags/4.2@770055 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/bmlt_tabs.js
+++ js/bmlt_tabs.js
@@ -1,59 +1,143 @@
-jQuery(document).ready(function() {
+jQuery(document).ready( function($) {
+    // drop down selection listener
 
-	jQuery('.bmlt_simple_meeting_one_meeting_address_td a').attr('target', '_blank');
+	$( "#tabs" ).tabs();
+	
+	var d = new Date();
+	
+	var n = d.getDay();
+	
+	$( "#tabs" ).tabs("select", n);
+	
+	$(".bmlt-table tr:even, .bmlt_simple_format_table tr:even").addClass("bmlt_alt_0");
+	
+	$(".bmlt-table tr:odd, .bmlt_simple_format_table tr:odd").addClass("bmlt_alt_0");
+	
+	$("#tabs").show();
 
-	jQuery('.bmlt_simple_format_table').prepend('<thead><th class="bmlt_simple_format_table_header">h1</th><th class="bmlt_simple_format_table_header">h1</th><th class="bmlt_simple_format_table_header">h1</th></thead>');
+	$( ".icon-map" ).button({ icons: { primary: "ui-icon-link" } });
 
-	jQuery('.bmlt_simple_meetings_table').prepend('<thead><th class="bmlt_simple_format_table_header">h0</th><th class="bmlt_simple_format_table_header">h1</th><th class="bmlt_simple_format_table_header">h2</th><th class="bmlt_simple_format_table_header">h3</th><th class="bmlt_simple_format_table_header">h5</th><th class="bmlt_simple_format_table_header">h4</th></thead>');
+	$( ".icon-format" ).button({ icons: { primary: "ui-icon-link" } });
 
-	jQuery('.bmlt_simple_format_table').dataTable( {
+	$(".showlegend").on('click', function(e){
+		e.preventDefault();
+        $( "#thislegend" ).removeClass("hide").addClass("show");
+		$( ".selector" ).dialog({ position: { my: "center", at: "center", of: window } });
+		$( "#thislegend" ).dialog({ height: 800, width: 700, hide: "fade", show: "fade", title: "Meeting Formats", closeOnEscape: true, modal: false, position: { my: "center", at: "center", of: window } });
+		$( "#thislegend" ).dialog({ close: function (event, ui) { $(this).dialog("destroy");$(this).removeClass("show").addClass("hide"); }});
+	});
 
-		"bPaginate": false,
+	$("#e2").select2({
+                placeholder: "Select a City",
+				dropdownAutoWidth: true,
+                allowClear: false,
+                width: "180px",
+				minimumResultsForSearch: 1
+     });
+	
+	$("#e3").select2({
+                placeholder: "Select a Home Group",
+				dropdownAutoWidth: true,
+                allowClear: false,
+                width: "180px",
+				minimumResultsForSearch: 1
+     });
+	
+	$("#e4").select2({
+                placeholder: "Select a Location",
+				dropdownAutoWidth: true,
+                allowClear: false,
+                width: "180px",
+				minimumResultsForSearch: 1
+     });
+	
+	$("#e2").on('click', function(){
+		$("#e3").select2("val", null);
+		$("#e4").select2("val", null);
+        // check the pages to find the page that is visible and hide it
+		var val = $("#e2").val();
+        $('.page').each(function(index) {
+            if(this.className.indexOf('show', 0) > -1){
+                $("#"+this.id).fadeOut(function(){
+                    $("#"+this.id).removeClass("show").addClass("hide");
+                    showPage(val);
+                });
+                // found the visible page now break out of the each loop.
+                return;
+            }
+        });
+   });
 
-		"bLengthChange": false,
+	$("#e3").on('click', function(){
+        // check the pages to find the page that is visible and hide it
+		$("#e2").select2("val", null);
+		$("#e4").select2("val", null);
+		var val = $("#e3").val();
+        $('.page').each(function(index) {
+            if(this.className.indexOf('show', 0) > -1){
+                $("#"+this.id).fadeOut(function(){
+                    $("#"+this.id).removeClass("show").addClass("hide");
+                    showPage(val);
+                });
+                // found the visible page now break out of the each loop.
+                return;
+            }
+        });
+   });
 
-		"bFilter": false,
+	$("#e4").on('click', function(){
+        // check the pages to find the page that is visible and hide it
+		$("#e2").select2("val", null);
+		$("#e3").select2("val", null);
+		var val = $("#e4").val();
+        $('.page').each(function(index) {
+            if(this.className.indexOf('show', 0) > -1){
+                $("#"+this.id).fadeOut(function(){
+                    $("#"+this.id).removeClass("show").addClass("hide");
+                    showPage(val);
+                });
+                // found the visible page now break out of the each loop.
+                return;
+            }
+        });
+   });
 
-		"bSort": true,
+	$("#day").on('click', function(){
 
-		"bInfo": false,
+		$("#e2").select2("val", null);
+		$("#e3").select2("val", null);
+		$("#e4").select2("val", null);
+	
+        // check the pages to find the page that is visible and hide it
+        $('.page').each(function(index) {
+            if(this.className.indexOf('show', 0) > -1){
+                $("#"+this.id).fadeOut(function(){
+                    $("#"+this.id).removeClass("show").addClass("hide");
+                    showPage("days");
+                });
+                // found the visible page now break out of the each loop.
+                return;
+            }
+        });
+   });
 
-		"bAutoWidth": false,
+    $('.custom-ul').on('click', 'a', function(event){
+        // check the pages to find the page that is visible and hide it
+        $('.page').each(function(index) {
+            if(this.className.indexOf('show', 0) > -1){
+                $("#"+this.id).fadeOut(function(){
+                    $("#"+this.id).removeClass("show").addClass("hide");
+                    showPage(event.target.id);
+                });
+                // found the visible page now break out of the each loop.
+                return;
+            }
+        });
+   });
 
-		"sDom": 'Rlfrtip',
-
-		"oColReorder": {
-
-			"aiOrder": [ 0, 1, 2 ]
-
-			}
-
-	} );
-
-	jQuery('.bmlt_simple_meetings_table').dataTable( {
-
-		"bPaginate": false,
-
-		"bLengthChange": false,
-
-		"bFilter": false,
-
-		"bSort": false,
-
-		"bInfo": false,
-
-		"bAutoWidth": false,
-
-		"sDom": 'Rlfrtip',
-
-		"oColReorder": {
-
-			"aiOrder": [ 2, 1, 3, 4, 0, 5 ]
-
-			}
-
-	} );
-
-	jQuery("#tabs").show();
+    // show the selected page
+    function showPage(thisTarget){
+        $("#"+thisTarget).fadeIn().removeClass("hide").addClass("show");
+    }
 
 });
